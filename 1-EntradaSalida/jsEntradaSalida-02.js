0d125310cdf4f67d92b2bc13d710af8ebdf519f7
germanzitterkopf <germanzitter@gmail.com>
2021-07-05 23:25:49 -0300

    5_7_21

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -2,7 +2,9 @@
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	alert("ok");
+{var nombreusuario;
+	nombreusuario = prompt( "german");
+	console.log (nombreusuario)
+	alert(nombreusuario);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,10 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
-}
+	var nombre,mensaje;
+    nombre=prompt("ingresa tu nombre");
+    mensaje= " su nombre es : " + nombre;
+    document.getElementById('txtIdNombre').value=mensaje;
 
+	//alert( mensaje );
+}
